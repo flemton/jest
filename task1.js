76d218143fbc9e9139f901d0fbdf6bf12d3c5928
Alhassan Osman <oalhassan847@gmail.com>
2023-01-25 16:15:08 +0000

    add string length condition and test

--- task1.js
+++ task1.js
@@ -1,4 +1,8 @@
 function stringLength(string) {
+    if (!(string.length >= 1 & string.length <= 10))
+    {
+        return ('error, string length less than 1 or greater than 10');
+    }
     return string.length; 
 }
 
