3c21de5429cdfa81e310c55c4d2ea58c41096275
Alhassan Osman <oalhassan847@gmail.com>
2023-01-25 16:42:28 +0000

    reverseString using JS functions split, reverse and join

--- task2.js
+++ task2.js
@@ -1,10 +1,5 @@
 function reverseString(string) {
-    let reverse = [];
-    for (let i = 0; i < string.length; i++)
-    {
-        reverse[i] = string[string.length - [i + 1]];
-    }
-    return reverse.join('');
+    return string.split('').reverse().join('');
 }
 
 module.exports = reverseString;
